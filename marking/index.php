fa68e9e8c56c1c21fa519895d0e784fda5ad71ba
loretolopezpino <loreto.lopezpino@gmail,com>
2021-12-15 14:16:54 -0300

    Notification no permission to view examen

--- marking/index.php
+++ marking/index.php
@@ -63,11 +63,7 @@ $context = context_module::instance($cm->id);
 // the emarking is hidden
 list ($issupervisor, $usercangrade) = emarking_get_grading_permissions($emarking, $context);
 
-if(
-    $usercangrade == false && $submission->student != $USER->id ||
-    $usercangrade == false && $submission->student == $USER->id && $draft->status < EMARKING_STATUS_PUBLISHED
-)
-{
+if($usercangrade == false && $submission->student != $USER->id){
     echo get_string("emptypermissions", "mod_emarking");
     return false;
 }
